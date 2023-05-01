e91217a9269d9371649991d0c32a2ed61282b875
samir.akhmedov <akhmedov@surf.dev>
2023-05-01 18:45:44 +0400

    Initial Commit

---
+++ emirates/emiratesApp.swift
@@ -0,0 +1,17 @@
+//
+//  emiratesApp.swift
+//  emirates
+//
+//  Created by akhmedov on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct emiratesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
